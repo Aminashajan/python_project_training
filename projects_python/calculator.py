a82b32af29deec1834de47cc825bbb3d5762f85e
Amina <aminashajanat@gmail.com>
2025-09-24 14:48:30 +0530

    24/9/25

--- projects_python/calculator.py
+++ projects_python/calculator.py
@@ -1,35 +1,51 @@
+print("==calculator==")
 
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mult(a,b):
-    return a*b
-def div(a,b):
-    return a//b
-def  mod(a,b):
-    return a%b
 
-a=int(input("enter the value of a"))
-b=int(input("enter the value of b"))
-choice=input("enter your choice[+,-,*,/,%]: ")
 
-if choice=="+":
-    print(add(a,b))
+final=""
 
-elif choice=="-":
-    print(sub(a,b))
+while(final!='q'):
+    a=int(input("enter the value of a"))
+    b=int(input("enter the value of b"))
+    choice=input("enter your choice[+,-,*,/,%]: ")
 
-elif choice=="*":
-    print(mult(a,b))
+    def add(a,b):
+        return a+b
+    def sub(a,b):
+        return a-b
+    def mult(a,b):
+        return a*b
+    def div(a,b):
+        return a//b
+    def  mod(a,b):
+        return a%b
+    
 
-elif choice=="/":
-    if b==0:
-        print("divide by zero is not allowed")
-    else:
-        print(div(a,b))
 
-elif choice=="%":
-    print(mod(a,b))
+    if choice=="+":
+        print(add(a,b))
+    elif choice=="-":
+        print(sub(a,b))
+    elif choice=="*":
+        print(mult(a,b))
+    elif choice=="/":
+        if b==0:
+            print("divide by zero is not allowed")
+        else:
+            print(div(a,b))
+    elif choice=="%":
+        print(mod(a,b))
+
+    final=input("do u want to quit[q] or resume[r]")
+
+else:
+    print("thank u")
+
+
+
+   
+
+
+
 
 
