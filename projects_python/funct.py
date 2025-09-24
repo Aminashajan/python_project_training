e1f57f842aa41aad935ccb149d1535bd5e730f6c
Amina <aminashajanat@gmail.com>
2025-09-24 12:06:34 +0530

    Add files via upload
    calculator_project:24/9/2025

---
+++ projects_python/funct.py
@@ -0,0 +1,34 @@
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def div(a,b):
+    return a//b
+def  mod(a,b):
+    return a%b
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+choice=input("enter your choice[+,-,*,/,%]: ")
+
+if choice=="+":
+    print(add(a,b))
+
+elif choice=="-":
+    print(sub(a,b))
+
+elif choice=="*":
+    print(mult(a,b))
+
+elif choice=="/":
+    if b==0:
+        print("divide by zero is not allowed")
+    else:
+        print(div(a,b))
+
+elif choice=="%":
+    print(mod(a,b))
+
