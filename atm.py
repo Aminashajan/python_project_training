8b8a0f847af72fb24d941b92e2a33baa0aeedcb8
Amina <aminashajanat@gmail.com>
2025-09-25 10:19:46 +0530

    25//9/25

---
+++ atm.py
@@ -0,0 +1,11 @@
+# count=0
+print("==WELCOME TO XYZ ATM==!!")
+while True:
+    pwsd=int(input("enter the password= "))
+    if pwsd==2004:
+        print("in process!!please wait")
+        break
+
+    else:
+        print("invalid !try again")
+    #count+=1
